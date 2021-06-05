f57f1d5088f72aeceec2bd02ceec9c8165c2c67c
Saywex89 <omg12121@mail.ru>
2021-06-05 08:49:44 +0700

    add new component site header

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header.jsx'
+
 
 function App() {
   return (
     <div className="App">
-      <div className='title_page'>
-        <h1>Main page</h1>
-      </div>
+      <Header />
+      
     </div>
   );
 }
