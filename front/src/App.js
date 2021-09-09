471bbf2b3c2be87a2901c11039b3fa48690861c4
Saywex89 <omg12121@mail.ru>
2021-09-10 01:53:11 +0700

    check auth from frontend

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Fetch } from 'react-request';
 import './App.css';
 import './components/News/News'
+import logo from './logo.svg';
 import Header from './components/Header/Header';
 
 
@@ -8,6 +10,17 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Fetch
+        url="http://127.0.0.1:8000/auth/users/me/"
+        onResponse={(error, response) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(JSON.stringify(response.data));
+          }
+        }}
+      >
+      </Fetch>
     </div>
   );
 }
